eecb8ca7a69e9840872a1fdf41520a13e845aa36
Andrei Shamshur <rushmash@gmail.com>
2021-12-10 12:40:27 +0300

    refactor day10

--- aoc2021/day10.py
+++ aoc2021/day10.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from itertools import *
+from collections import deque
 
 lines = [l.strip() for l in open('in10.txt', 'r')]
 
@@ -31,25 +32,24 @@ token_pair = {
 def part1():
 	score = 0
 	for line in lines:
-		brackets = []
+		brackets = deque()
 		for token in line:
-			match token:
-				case ('('|'['|'{'|'<'):
-					brackets.append(token)
-				case (')'|']'|'}'|'>'):
-					if token_pair[token] != brackets.pop():
-						score += score_points1[token]
+			if token in ['(', '[', '{', '<']:
+				brackets.append(token)
+			elif token in [')', ']', '}', '>']:
+				if token_pair[token] != brackets.pop():
+					score += score_points1[token]
+					break
 	return score
 
 def get_incomplete_brackets(line):
-	brackets = []
+	brackets = deque()
 	for token in line:
-		match token:
-			case ('('|'['|'{'|'<'):
-				brackets.append(token)
-			case (')'|']'|'}'|'>'):
-				if token_pair[token] != brackets.pop():
-					return []
+		if token in ['(', '[', '{', '<']:
+			brackets.append(token)
+		elif token in [')', ']', '}', '>']:
+			if token_pair[token] != brackets.pop():
+				return []
 	return brackets
 
 def part2():
@@ -65,7 +65,7 @@ def part2():
 			score += score_points2[token_pair[b]]
 		scores.append(score)
 
-	return sorted(scores)[(len(scores) - 1)//2]
+	return sorted(scores)[len(scores)//2]
 
 print(part1())
 print(part2())
