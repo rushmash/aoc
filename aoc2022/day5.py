99c2fed1c3fa90947ceb6381e71d036f9ae67634
Andrei Rushmash <rushmash@gmail.com>
2022-12-06 06:32:17 +0300

    fix day5 parse stacks part from input data

--- aoc2022/day5.py
+++ aoc2022/day5.py
@@ -2,38 +2,29 @@
 import re
 import copy
 
+def transpose(M):
+    size = max([len(s) for s in M])
+    T = [[] for _ in range(size)]
+
+    for line in M:
+        for vi, v in enumerate(line):
+            if v:
+                T[vi].append(v)
+
+    return list(map(lambda x: list(reversed(x)), T))
+
 data = open('in5.txt', 'r').read().split('\n\n')
-#data_stacks = data[0].splitlines()
+data_stacks = data[0].splitlines()
 data_instr  = data[1].splitlines()
 
-# p1 = re.compile('\w')
-# stacks = list(map(p1.findall, data_stacks))
-# stacks.pop()
+p1 = re.compile('(\w)|\s{4}')
+stacks = list(map(p1.findall, data_stacks))
+stacks.pop()
+
+stacks = transpose(stacks)
 
 p2 = re.compile('\d+')
 instructions = list(map(lambda x: list(map(int, p2.findall(x))), data_instr))
-
-# [G]                 [D] [R]
-# [W]         [V]     [C] [T] [M]
-# [L]         [P] [Z] [Q] [F] [V]
-# [J]         [S] [D] [J] [M] [T] [V]
-# [B]     [M] [H] [L] [Z] [J] [B] [S]
-# [R] [C] [T] [C] [T] [R] [D] [R] [D]
-# [T] [W] [Z] [T] [P] [B] [B] [H] [P]
-# [D] [S] [R] [D] [G] [F] [S] [L] [Q]
-#  1   2   3   4   5   6   7   8   9
-
-stacks = [
-    ['D', 'T', 'R', 'B', 'J', 'L', 'W', 'G'],
-    ['S', 'W', 'C'],
-    ['R', 'Z', 'T', 'M'],
-    ['D', 'T', 'C', 'H', 'S', 'P', 'V'],
-    ['G', 'P', 'T', 'L', 'D', 'Z'],
-    ['F', 'B', 'R', 'Z', 'J', 'Q', 'C', 'D'],
-    ['S', 'B', 'D', 'J', 'M', 'F', 'T', 'R'],
-    ['L', 'H', 'R', 'B', 'T', 'V', 'M'],
-    ['Q', 'P', 'D', 'S', 'V']
-]
 
 def stack_top(s) -> str:
     return ''.join(map(lambda x: x[-1], s))
