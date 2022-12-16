e26ed2c6c28a986f474ee78ab4f40c2592fa96f5
Andrei Shamshur <rushmash@gmail.com>
2022-12-16 15:04:52 +0300

    refactor day20 of aoc2021

--- aoc2021/day20.py
+++ aoc2021/day20.py
@@ -4,10 +4,10 @@ data = open('in20.txt').read().split('\n\n')
 algorithm_data = data[0]
 image_data = [list(x) for x in data[1].splitlines()]
 
-MAX_ENHANCEMENT = 100
+MAX_ENHANCEMENT = 50
 
 def count_lit_pixels(G):
-    pixels, (max_x, max_y, min_x, min_y) = G
+    pixels, (min_x, min_y, max_x, max_y) = G
     num = 0
     for (x, y) in pixels:
         if min_x <= x <= max_x and min_y <= y <= max_y:
@@ -15,10 +15,12 @@ def count_lit_pixels(G):
     return num
 
 def enhance(G):
-    pixels, (max_x, max_y, min_x, min_y) = G
+    pixels, (min_x, min_y, max_x, max_y) = G
     enhanced = set()
-    for x in range(min_x-1-MAX_ENHANCEMENT*2, max_x+2+MAX_ENHANCEMENT*2):
-        for y in range(min_y-1-MAX_ENHANCEMENT*2, max_y+2+MAX_ENHANCEMENT*2):
+    padding = MAX_ENHANCEMENT*2
+
+    for x in range(min_x-1-padding, max_x+2+padding):
+        for y in range(min_y-1-padding, max_y+2+padding):
             v = ''
             for xx, yy in [(x-1,y-1),(x-1,y),(x-1,y+1),(x,y-1),(x,y),(x,y+1),(x+1,y-1),(x+1,y),(x+1,y+1)]:
                 v += '1' if (xx, yy) in pixels else '0'
@@ -26,19 +28,17 @@ def enhance(G):
             if algorithm_data[int(v, 2)] == '#':
                 enhanced.add((x,y))
 
-    return enhanced, (max_x+1, max_y+1, min_x-1, min_y-1) 
+    return enhanced, (min_x-1, min_y-1, max_x+1, max_y+1)
 
 pixels = set()
-max_x, max_y, min_x, min_y = len(image_data), len(image_data[0]), 0, 0
-
 for ix, x in enumerate(image_data):
     for iy, y in enumerate(x):
         if y == '#':
             pixels.add((ix, iy))
+image = (pixels, (0, 0, len(image_data), len(image_data[0])))
 
-image = (pixels, (max_x, max_y, min_x, min_y))
-
-for i in range(MAX_ENHANCEMENT):
+for i in range(1, MAX_ENHANCEMENT+1):
     image = enhance(image)
-    if i+1 == 2 or i+1 == 50:
+
+    if i == 2 or i == 50:
         print(count_lit_pixels(image))
